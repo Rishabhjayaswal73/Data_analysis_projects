36137f85ad54de58a20682abc32befd984119e6d
Rishabh Jayaswal <rishabhjayaswal73@gmail.com>
2026-03-17 22:04:25 +0530

    Added all projects

---
+++ Electric vehical project/SQL/users.sql
@@ -0,0 +1,12 @@
+Create table Users
+(
+	id int unsigned,
+    name varchar(100),
+    email varchar(150),
+    password varchar(100),
+    contact varchar(15),
+    address text,
+    dob date,
+    gender enum("M","F","O"),
+    status boolean 
+)
